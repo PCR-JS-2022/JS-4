b264b65288761ab28c7ccb22d45ac097a9c9c967
EgorNmv <43643833+EgorNmv@users.noreply.github.com>
2022-03-31 16:07:33 +0500

    Добавлены тесты

---
+++ __tests__/test.js
@@ -0,0 +1,71 @@
+const { Company, ExchangeObserver, Member } = require("../index");
+const assert = require("assert");
+
+describe("ExchangeObserver", () => {
+  it("Объект корректно создается", () => {
+    const exchange = new ExchangeObserver();
+
+    assert.equal(typeof exchange.sellShares, "function");
+    assert.equal(typeof exchange.updateCompany, "function");
+    assert.equal(typeof exchange.onUpdateCompany, "function");
+  });
+});
+
+describe("Company", () => {
+  it("Объект корректно создается", () => {
+    const exchange = new ExchangeObserver();
+    const company = new Company(exchange, "Green Bank", 100, 100);
+
+    assert.equal(company.name, "Green Bank");
+    assert.equal(company.currentSharePrice, 100);
+    assert.equal(company.exchangeObserver instanceof ExchangeObserver, true);
+    assert.equal(company.shareCount, 100);
+    assert.equal(typeof company.updatePrice, "function");
+  });
+});
+
+describe("Member", () => {
+  it("Объект корректно создается", () => {
+    const exchange = new ExchangeObserver();
+    const company = new Company(exchange, "Green Bank", 100, 100);
+    const member = new Member(exchange, 10000, [company], 10);
+
+    assert.equal(member.exchangeObserver instanceof ExchangeObserver, true);
+    assert.equal(member.balance, 10000);
+    assert.deepEqual(member.interestingCompanies, [company]);
+    assert.deepEqual(member.purchasedSharesNumber, 10);
+  });
+
+  it("Вовремя покупает акции одной компании (1)", () => {
+    const exchange = new ExchangeObserver();
+    const company = new Company(exchange, "Green Bank", 100, 100);
+    const member = new Member(exchange, 10000, [company], 10);
+
+    company.updatePrice(70);
+    company.updatePrice(73);
+    assert.equal(company.shareCount, 90);
+    assert.equal(member.balance, 10000 - 73 * 10);
+  });
+
+  it("Корректно обрабатывает исключение при попытке купить большее кол-во акций, чем есть у компании", () => {
+    assert.throws(() => {
+      const exchange = new ExchangeObserver();
+      const company = new Company(exchange, "Green Bank", 100, 100);
+      const member = new Member(exchange, 10000, [company], 1000);
+
+      company.updatePrice(70);
+      company.updatePrice(73);
+    });
+  });
+
+  it("Корректно обрабатывает исключение при попытке купить акций на сумму превышающюю баланс", () => {
+    assert.throws(() => {
+      const exchange = new ExchangeObserver();
+      const company = new Company(exchange, "Green Bank", 100, 100);
+      const member = new Member(exchange, 10, [company], 1);
+
+      company.updatePrice(70);
+      company.updatePrice(73);
+    });
+  });
+});
--- index.js
+++ index.js
@@ -3,67 +3,55 @@
  */
 
 /** Класс биржи */
-export class Exchange {
+class Exchange {
   /**
    * Создаёт экзмепляр биржи
    * @param {Object<string, Array<listenerCallBack>} listeners - словарь, в котором ключи - названия компаний,
    * а значения - функции, которые вызываются при изменении цены акции этой компании
    */
-  constructor(listeners) {
-
-  }
+  constructor(listeners) {}
 
   /**
    * Метод, осуществляющий продажу акций компании участнику биржи
-   * @param {Company} company 
-   * @param {Member} member 
+   * @param {Company} company
+   * @param {Member} member
    */
-  sellShares(company, member) {
-
-  }
+  sellShares(company, member) {}
 
   /**
    * Метод, уведомляющий всех подписчиков компании об изменениях
-   * @param {Company} company 
+   * @param {Company} company
    */
-  updateCompany(company) {
-
-  }
+  updateCompany(company) {}
 
   /**
    * Метод, позволяющий подписаться на уведомления об изменениях компании
    * @param {string} companyName
    * @param {listenerCallBack} cb
    */
-  onUpdateCompany(companyName, cb) {
-
-  }
+  onUpdateCompany(companyName, cb) {}
 }
 
 /** Класс компании */
-export class Company {
+class Company {
   /**
    * Создаёт экзмепляр компании
-   * @param {string} name - название компании
-   * @param {number} [sharePrice = 0] - цена акции за штуку
    * @param {Exchange} exchangeObserver - объект биржи, на которой торгует компания
+   * @param {string} name - название компании
    * @param {number} [shareCount = 0] - количество акций компании, выставленных на продажу
+   * @param {number} [sharePrice = 0] - цена акции за штуку
    */
-  constructor(name, sharePrice, exchangeObserver, shareCount) {
-
-  }
+  constructor(name, sharePrice, exchangeObserver, shareCount) {}
 
   /**
    * Метод, обновляющий цену акции компании
    * @param {number} newPrice
    */
-  updatePrice(newPrice) {
-
-  }
+  updatePrice(newPrice) {}
 }
 
 /** Класс участника торгов */
-export class Member {
+class Member {
   /**
    * Создаёт экзмепляр участника торгов
    * @param {Exchange} exchangeObserver - объект биржи
@@ -71,7 +59,12 @@ export class Member {
    * @param {Company[]} [interestingCompanies = []] - компании, за акциями которых участнику было бы интересно следить
    * @param {number} [purchasedSharesNumber = 10] - количество акций компании, выставленных на продажу
    */
-  constructor(exchangeObserver, balance, interestingCompanies, purchasedSharesNumber) {
-
-  }
+  constructor(
+    exchangeObserver,
+    balance,
+    interestingCompanies,
+    purchasedSharesNumber
+  ) {}
 }
+
+module.exports = { Exchange, Company, Member };
