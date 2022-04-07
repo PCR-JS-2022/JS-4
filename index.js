a3bef132d3698db7c870befd96f9dee08785681e
danichka <daniika.ella@gmail.com>
2022-04-07 21:20:40 +0500

    ферст трай

--- index.js
+++ index.js
@@ -9,27 +9,53 @@ class ExchangeObserver {
    * @param {Object<string, Array<listenerCallBack>} listeners - словарь, в котором ключи - названия компаний,
    * а значения - функции, которые вызываются при изменении цены акции этой компании
    */
-  constructor(listeners) {}
+  constructor(listeners) {
+    if (typeof listeners !== 'object')
+			throw new Error('Некорректный словарь listeners')
+		this.listeners = listeners
+  }
+
 
   /**
    * Метод, осуществляющий продажу акций компании участнику биржи
    * @param {Company} company
    * @param {Member} member
    */
-  sellShares(company, member) {}
+  sellShares(company, member) {
+    if (typeof name !== 'string' || !name)
+			throw new Error('Некорректное имя компании')
+
+    if (member.purchasedSharesNumber > company.shareCount)
+      throw new Error('Некорректное кол-во акций');
+
+    const transaction = member.purchasedSharesNumber * company.sharePrice;
+    if (transaction > member.balance)
+      throw new Error('Недостаточно средств для покупки');
+
+    member.balance -= transaction;
+    company.shareCount -= member.purchasedSharesNumber;
+  }
 
   /**
    * Метод, уведомляющий всех подписчиков компании об изменениях
    * @param {Company} company
    */
-  updateCompany(company) {}
+  updateCompany(company) {
+    this.listeners.get(company.name).forEach(
+      (x) => x(company));
+  }
 
   /**
    * Метод, позволяющий подписаться на уведомления об изменениях компании
    * @param {string} companyName
    * @param {listenerCallBack} cb
    */
-  onUpdateCompany(companyName, cb) {}
+  onUpdateCompany(companyName, cb) {
+    if (!this.listeners.has(companyName))
+      this.listeners.set(companyName, []);
+
+    this.listeners.get(companyName).push(cb);
+  }
 }
 
 /** Класс компании */
@@ -41,13 +67,23 @@ class Company {
    * @param {number} [shareCount = 0] - количество акций компании, выставленных на продажу
    * @param {number} [sharePrice = 0] - цена акции за штуку
    */
-  constructor(exchangeObserver, name, shareCount, sharePrice) {}
+  constructor(exchangeObserver, name, shareCount, sharePrice) {
+    this.exchangeObserver = exchangeObserver;
+    this.name = name;
+    this.shareCount = shareCount;
+    this.sharePrice = sharePrice;
+  }
 
   /**
    * Метод, обновляющий цену акции компании
    * @param {number} newPrice
    */
-  updatePrice(newPrice) {}
+  updatePrice(newPrice) {
+    const oldPrice = this.sharePrice;
+    this.sharePrice = newPrice;
+    if (newPrice > oldPrice && this.shareCount > 0) 
+      this.exchangeObserver.updateCompany(this);  
+  }
 }
 
 /** Класс участника торгов */
@@ -64,7 +100,20 @@ class Member {
     balance,
     interestingCompanies,
     purchasedSharesNumber
-  ) {}
+  ) {
+    this.exchangeObserver = exchangeObserver;
+    this.balance = balance;
+    this.interestingCompanies = interestingCompanies;
+    this.purchasedSharesNumber = purchasedSharesNumber;
+
+    function assignCB(company) {
+      exchangeObserver.onUpdateCompany(company.name, (company) =>  
+      exchangeObserver.sellShares(company, this))
+    }
+
+    interestingCompanies.forEach(
+      x => assignCB.bind(this)(x));
+  }
 }
 
 module.exports = { ExchangeObserver, Company, Member };
